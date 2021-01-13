c36d18f57d494908bee3f2d00b4d33aba937c56e
GitHub Action <action@github.com>
2021-01-13 21:40:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/tkulzer/tmatableviewer/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tkulzer/tmatableviewer/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tkulzer.tmatableviewer.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tkulzer.tmatableviewer.MyBundle
 
 class MyApplicationService {
 
